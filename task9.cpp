1af9243973fec8b3e4fea7a2f46cf9f15e96b415
Raveeha Mohsin <mohsinraveeha@gmail.com>
2022-12-18 02:29:30 +0500

    message

---
+++ task9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main()
+
+{
+system ("Color 70");
+cout << endl;
+cout << "  ---------------------------  " <<endl;
+cout << "    o    ^____^                  " <<endl;
+cout << "      o   (oo) \\______________   " <<endl;
+cout << "          (__) \\             )\\/\\   " <<endl;
+cout << "               ||-----------w |         " <<endl;
+cout << "               ||            ||          " <<endl;
+
+}
+
+
+
